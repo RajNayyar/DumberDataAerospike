3bd261cd77f10e313913e88831a017023bbc2d40
Rajpreet Singh Nayyar <rnayyar@tavisca.com>
2018-09-29 18:49:02 +0530

    implemented Aerospike and api

---
+++ AerospikeTweetApi/AerospikeTweetApi/Models/tweet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AerospikeTweetApi.Models
+{
+    public class Tweet
+    {
+        public string author;
+
+        public string tweetId{get; set;}
+
+        public string tweetText { get; set; }
+        public string UserFollower { get; internal set; }
+        public string UserFollowing { get; internal set; }
+    }
+}
